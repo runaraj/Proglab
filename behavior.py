45d09311070dcd32f4a80cfd71afd2929e72b708
Runar <runar.johannessen96@gmail.com>
2016-10-31 13:52:08 +0100

    Behavior

--- behavior.py
+++ behavior.py
@@ -14,14 +14,33 @@ class Behavior:
         self.match_degree = 0
         self.weight = 0
 
-    def consider_deavtivation(self):
+    def set_priority(self, value):
+        self.priority = value
+
+    def consider_deactivation(self):
         pass
 
     def consider_activation(self):
         pass
 
     def update(self):
-        pass
+        if active_flag:
+            self.consider_deactivation()
+        else:
+            self.consider_activation()
+        if active_flag:
+            self.sense_and_act()
+
 
     def sense_and_act(self):
         pass
+
+    def calculate_weight(self):
+        self.weight = self.priority*self.match_degree
+        return self.weight
+
+
+
+class CollisionAvoidanceBehavior(Behavior):
+
+    
