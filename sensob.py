1be4aea68da977f1274f656fdc949fed35307fcc
matsjs <matsjorgen@hotmail.com>
2016-10-31 12:36:45 +0100

    Updated class

--- sensob.py
+++ sensob.py
@@ -1,8 +1,16 @@
 class sensob:
     sensors = [] #One or more of the sensors associated with this class
-    sensor_values = None #values associated with the sensors
+    sensor_values = [] #values associated with the sensors
 
     def update(self): #force the sensob to fetch the relevant sensor value(s) and convert them into the pre-processed sensob value
-        pass
+        self.sensor_values.clear()
+        for sensor in self.sensors:
+            self.sensor_values.append(sensor.update)
+
     def get_values(self):
-        pass
+        return self.sensor_values
+
+
+    def set_sensors(self, newSensors):
+        for sens in newSensors:
+            self.sensors.append(sens)
