8b0c85225907cb61f511370ca2c953b24a29bb84
Runar <runar.johannessen96@gmail.com>
2016-10-31 11:54:42 +0100

    motob setup

--- motob.py
+++ motob.py
@@ -4,8 +4,14 @@ from motors import Motors
 class Motob:
 
     def __init__(self):
-        motor = Motors()
+        motors = [] #list of motors whose settings will be determined
+        value = None #a holder of the most recent MR sent to the motob
+        #value = (direction, degree) e.g (L, 30)=> turn 30 degrees to the left
 
 
-    def set_value(val):
+    def update(mr): #recv. a new MR, load it into value slot, and operate
+        self.value = mr
+        self.operationalize()
+
+    def operationalize(): #convert a MR into 1 or more motor settings, and send them to corresponding motor
         pass
