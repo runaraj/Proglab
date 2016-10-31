2f9b63b5b444cd43aefb89cdbcd9b91031fee3c0
matsjs <matsjorgen@hotmail.com>
2016-10-31 11:16:17 +0100

    Updated class

--- basic_robot/sensob.py
+++
@@ -1,8 +0,0 @@
-class sensob:
-    sensors = [] #One or more of the sensors associated with this class
-    sensor_values = None #values associated with the sensors
-
-    def update(self): #force the sensob to fetch the relevant sensor value(s) and convert them into the pre-processed sensob value
-        pass
-    def get_values(self):#
-        pass
