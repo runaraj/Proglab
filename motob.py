7eb50640c297901926d5feb602f66c23fd9cebb5
Andrefei <andreas.eikeland@hotmail.com>
2016-11-02 12:17:45 +0100

    Changed from list of motors to one object

--- motob.py
+++ motob.py
@@ -4,7 +4,7 @@ from motors import Motors
 class Motob:
 
     def __init__(self):
-        self.motors = []    # list of motors whose settings will be determined
+        self.motors = []    # The motor object we control. The motor object controls both weels
         # TODO change this from list to one object
         self.value = None   # a holder of the most recent MR sent to the motob
         # value = (direction, degree) e.g (L, 30)=> turn 30 degrees to the left
