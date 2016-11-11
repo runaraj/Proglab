7777d0111d2908d72f08dcb53fb72852e03a0884
Andrefei <andreas.eikeland@hotmail.com>
2016-11-11 12:16:26 +0100

    Fine tuned "turns", motob should now be able to transfer MR to MS

--- motob.py
+++ motob.py
@@ -3,7 +3,7 @@ from motors import Motors
 
 class Motob:
 
-    def __init__(self, motor, duration=1):
+    def __init__(self, motor, duration=0.5):
         self.motor = motor    # The motor object we control. The motor object controls both wheels
         self.value = None   # a holder of the most recent MR sent to the Motob
         # value = (direction, degree) e.g (L, 30)=> turn 30 degrees to the left
@@ -13,36 +13,34 @@ class Motob:
         self.value = mr
         self.operationalize()
 
-    def operationalize(self):  # convert a MR into 1 or more motor settings, and send them to corresponding motor
-        if self.value[0] == 'L':
-            #  self.motor.set_left_dir(self.value[1]) Don't think this is needed?
-            if self.value[1] == 10:
-                self.motor.set_value((0.3, 0.4), self.duration / 2)
+    def operationalize(self):  # Convert a MR into 1 or more motor settings, and send them to corresponding motor
+        if self.value[0] == 'L':  # 15, 30, 90 serves as indicators on how much the robot "turns"
             if self.value[1] == 15:
-                self.motor.set_value((0.3, 0.4), self.duration)
+                self.motor.set_value((-0.3, 0.3), self.duration/2)
             if self.value[1] == 30:
-                self.motor.set_value((0.4, 0.9), self.duration / 2)
+                self.motor.set_value((-0.3, 0.3), self.duration)
             if self.value[1] == 90:
-                self.motor.set_value((0.4, 0.9), self.duration)
-            self.motor.forward(dur=self.duration / 2)
+                self.motor.set_value((-0.4, 0.4), self.duration * 1.85)
 
         if self.value[0] == 'R':
-            # self.motor.set_right_dir(self.value[1])
-            if self.value[1] == 10:
-                self.motor.set_value((0.4, 0.3), self.duration/2)
             if self.value[1] == 15:
-                self.motor.set_value((0.4, 0.3), self.duration)
+                self.motor.set_value((0.3, -0.3), self.duration/2)
             if self.value[1] == 30:
-                self.motor.set_value((0.9, 0.4), self.duration/2)
+                self.motor.set_value((0.3, -0.3), self.duration)
             if self.value[1] == 90:
-                self.motor.set_value((0.9, 0.4), self.duration)
-            self.motor.forward(dur=self.duration)
+                self.motor.set_value((0.4, -0.4), self.duration * 1.85)
 
         if self.value[0] == "F":
-            self.motor.forward(dur=self.duration)
+            if self.value[1] != 0:
+                self.motor.forward(dur=self.value[1])
+            else:
+                self.motor.forward(dur=self.duration)
 
         if self.value[0] == "B":
-            self.motor.backward(dur=self.duration)
+            if self.value[1] != 0:
+                self.motor.backward(dur=self.value[1])
+            else:
+                self.motor.backward(dur=self.duration)
 
         if self.value[0] == "S":
             self.motor.stop()
